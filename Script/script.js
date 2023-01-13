86284c044b97319d84b679162579b305dfe9ec8b
Erik Oliveira <erikoliveira818@gmail.com>
2023-01-12 22:38:24 -0300

    2th Commit

---
+++ Script/script.js
@@ -0,0 +1,27 @@
+const noveNoveOnze = {
+    marca: 'Porshe',
+    modelo: '991 Turbo s',
+}
+
+const m3E46GTR = {
+    marca:'BMW',
+    modelo:'M3 E46 GTR',
+}
+const rs5SB = {
+    marca:'Audi',
+    modelo:'RS 5 Sportback',
+}
+
+
+const skylineR34GTR = {
+    marca:'Nissan',
+    modelo:'Skyline R34 GTR',
+}
+const supraMK4 = {
+    marca:'Toyota',
+    modelo:'Supra MK4',
+}
+const silvia = {
+    marca:'Nissan',
+    modelo:'Nissan',
+}
